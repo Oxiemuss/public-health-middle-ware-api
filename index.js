0fb279e6fb3a1544d2b2dc2938af140224f16e3f
Oxiemuss <oakrapon13@gmail.com>
2026-03-18 13:48:22 +0700

    add vercel config

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get("/", (req, res) => {
+    res.send("Public Health Middleware API is running...");
+});
 // เรียกใช้งาน Routes ทั้งหมด
 app.use("/api/healthcenter", healthRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/refer", referRouters);
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5500;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+// Export app เพื่อให้ Vercel เอาไปใช้งานแบบ Serverless
+module.exports = app;
